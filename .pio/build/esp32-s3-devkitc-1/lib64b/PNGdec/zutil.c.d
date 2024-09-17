2d79db76d5a97dfc714991116b24a3a27a44ff45
EgnionInnovation <fareed.ahmad@egnion.com>
2024-09-17 11:59:16 +0500

    v1.10 - added kW fields in progress message

---
+++ .pio/build/esp32-s3-devkitc-1/lib64b/PNGdec/zutil.c.d
@@ -0,0 +1,5 @@
+.pio/build/esp32-s3-devkitc-1/lib64b/PNGdec/zutil.c.o: \
+ .pio/libdeps/esp32-s3-devkitc-1/PNGdec/src/zutil.c \
+ .pio/libdeps/esp32-s3-devkitc-1/PNGdec/src/zutil.h \
+ .pio/libdeps/esp32-s3-devkitc-1/PNGdec/src/zlib.h \
+ .pio/libdeps/esp32-s3-devkitc-1/PNGdec/src/zconf.h
--- .pio/build/esp32-s3-devkitc-1/libb7c/PNGdec/zutil.c.d
+++
@@ -1,5 +0,0 @@
-.pio/build/esp32-s3-devkitc-1/libb7c/PNGdec/zutil.c.o: \
- .pio/libdeps/esp32-s3-devkitc-1/PNGdec/src/zutil.c \
- .pio/libdeps/esp32-s3-devkitc-1/PNGdec/src/zutil.h \
- .pio/libdeps/esp32-s3-devkitc-1/PNGdec/src/zlib.h \
- .pio/libdeps/esp32-s3-devkitc-1/PNGdec/src/zconf.h
