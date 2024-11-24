3a3ee0695527906d6615ddae2117a66c40acb0eb
EgnionInnovation <fareed.ahmad@egnion.com>
2024-11-24 14:30:27 +0500

    v1.17 - fixed CP State roles in starting and stopping charging

--- .pio/build/esp32-s3-devkitc-1/FrameworkArduino/cbuf.cpp.d
+++ .pio/build/esp32-s3-devkitc-1/FrameworkArduino/cbuf.cpp.d
@@ -1,3 +1,3 @@
 .pio/build/esp32-s3-devkitc-1/FrameworkArduino/cbuf.cpp.o: \
- /Users/fareedahmad/.platformio/packages/framework-arduinoespressif32@3.20011.230801/cores/esp32/cbuf.cpp \
- /Users/fareedahmad/.platformio/packages/framework-arduinoespressif32@3.20011.230801/cores/esp32/cbuf.h
+ /Users/fareedahmad/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ /Users/fareedahmad/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
