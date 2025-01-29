0b72bb3d0f8b9c4a1b3db0e7dff9d33ac193f128
EgnionInnovation <fareed.ahmad@egnion.com>
2025-01-29 20:48:18 +0500

    v1.26 - display fixed and QR Code feature added

--- .pio/build/esp32-s3-devkitc-1/lib64b/PNGdec/inffast.c.d
+++
@@ -1,8 +0,0 @@
-.pio/build/esp32-s3-devkitc-1/lib64b/PNGdec/inffast.c.o: \
- .pio/libdeps/esp32-s3-devkitc-1/PNGdec/src/inffast.c \
- .pio/libdeps/esp32-s3-devkitc-1/PNGdec/src/zutil.h \
- .pio/libdeps/esp32-s3-devkitc-1/PNGdec/src/zlib.h \
- .pio/libdeps/esp32-s3-devkitc-1/PNGdec/src/zconf.h \
- .pio/libdeps/esp32-s3-devkitc-1/PNGdec/src/inftrees.h \
- .pio/libdeps/esp32-s3-devkitc-1/PNGdec/src/inflate.h \
- .pio/libdeps/esp32-s3-devkitc-1/PNGdec/src/inffast.h
--- .pio/build/esp32-s3-devkitc-1/lib64b/PNGdec/inftrees.c.d
+++
@@ -1,6 +0,0 @@
-.pio/build/esp32-s3-devkitc-1/lib64b/PNGdec/inftrees.c.o: \
- .pio/libdeps/esp32-s3-devkitc-1/PNGdec/src/inftrees.c \
- .pio/libdeps/esp32-s3-devkitc-1/PNGdec/src/zutil.h \
- .pio/libdeps/esp32-s3-devkitc-1/PNGdec/src/zlib.h \
- .pio/libdeps/esp32-s3-devkitc-1/PNGdec/src/zconf.h \
- .pio/libdeps/esp32-s3-devkitc-1/PNGdec/src/inftrees.h
