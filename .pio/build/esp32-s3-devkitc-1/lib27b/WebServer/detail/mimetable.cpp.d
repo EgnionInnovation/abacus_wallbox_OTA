1399dd8564a19b1d56632061a858c2f3319885b8
Abdullah-python <Abdullah55980@gmail.com>
2025-03-26 01:32:09 +0500

    .pio

--- .pio/build/esp32-s3-devkitc-1/lib27b/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32-s3-devkitc-1/lib27b/WebServer/detail/mimetable.cpp.o: \
- /Users/fareedahmad/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
- /Users/fareedahmad/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
- /Users/fareedahmad/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
